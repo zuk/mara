9edfec4b5cb9d3da79fd9e4e72f14269c28dbf3c
zuk <zuk@roughest.net>
2009-05-22 17:55:08 -0400

    stretching clock sound

--- lib/mara.rb
+++ lib/mara.rb
@@ -8,17 +8,55 @@ class Mara < Processing::App
     smooth
 
     @font = create_font("Calibri", 80, true)
+    @timesize = 300
+    @timeloc = { :x => 265, :y => 355 }
+    @timewidth = 0
+    @mousestart = 0
+    @mousedrag = 0
+    @prev = 0
   end
   
   def draw
     background 50
-    translate 255, 255
     fill 255
 
-    timestring = Time.now.strftime("%I:%M:%S %p").gsub(/^0/,'')
+    case
+    when @timesize >= 250
+      timeformat = "%I:%M:%S %p"
+    when @timesize >= 125
+      timeformat = "%I:%M %p"
+    else
+      timeformat = "%H:%M"
+    end
+    
+    timestring = Time.now.strftime(timeformat).gsub(/^0/,'')
 
     text_font @font, 80
-    text timestring, 10, 100
+
+    @timewidth = text_width(timestring)
+    text timestring, @timeloc[:x], @timeloc[:y]
+    text @mousestart, 50, 100
+    text @mousedrag, 300, 100
+    text @mousedrag - @mousestart, 500, 100
+    text @timesize, 50,200
+  end
+
+  def mouse_pressed
+    @prev = pmouse_x
+  end
+
+  def mouse_dragged
+    #@timesize += pmouse_x - @mousestart
+    #
+    #
+    @timesize += pmouse_x - @prev
+    @timesize = 300 if @timesize >= 300
+    @timesize = 0 if @timesize <= 0
+    @prev = pmouse_x
+  end
+
+  def mouse_released
+    #@timesize = pmouse_x - @mousestart
   end
   
 end

3bb9b328eae7249d87a3fe856df165805dcd06e9
zuk <zuk@roughest.net>
2009-05-25 16:57:31 -0400

    sound of wolf howling

--- lib/mara.rb
+++ lib/mara.rb
@@ -1,6 +1,9 @@
 # Mara
+require 'net/http'
+require 'uri'
 
 class Mara < Processing::App
+  load_library 'moon'
 
   def setup
     frame_rate 30
@@ -9,21 +12,22 @@ class Mara < Processing::App
 
     @font = create_font("Calibri", 80, true)
     @timesize = 300
-    @timeloc = { :x => 265, :y => 355 }
+    @timeloc = { :x => 665, :y => 355 }
     @timewidth = 0
     @mousestart = 0
     @mousedrag = 0
     @prev = 0
+    @moon_phase = get_moon_phase
   end
   
   def draw
-    background 50
+    background 0
     fill 255
 
     case
     when @timesize >= 250
       timeformat = "%I:%M:%S %p"
-    when @timesize >= 125
+    when @timesize >= 80
       timeformat = "%I:%M %p"
     else
       timeformat = "%H:%M"
@@ -31,14 +35,17 @@ class Mara < Processing::App
     
     timestring = Time.now.strftime(timeformat).gsub(/^0/,'')
 
+    
     text_font @font, 80
 
     @timewidth = text_width(timestring)
+    text_align RIGHT
     text timestring, @timeloc[:x], @timeloc[:y]
-    text @mousestart, 50, 100
-    text @mousedrag, 300, 100
-    text @mousedrag - @mousestart, 500, 100
+    text_align LEFT
+
     text @timesize, 50,200
+
+    draw_moon
   end
 
   def mouse_pressed
@@ -46,10 +53,7 @@ class Mara < Processing::App
   end
 
   def mouse_dragged
-    #@timesize += pmouse_x - @mousestart
-    #
-    #
-    @timesize += pmouse_x - @prev
+    @timesize -= pmouse_x - @prev
     @timesize = 300 if @timesize >= 300
     @timesize = 0 if @timesize <= 0
     @prev = pmouse_x
@@ -57,6 +61,24 @@ class Mara < Processing::App
 
   def mouse_released
     #@timesize = pmouse_x - @mousestart
+  end
+
+  private
+  def get_moon_phase
+    p = Astro::Moon.phase
+puts p.inspect
+    p.illumination.to_f
+  end
+
+  def draw_moon
+    size = 100
+    
+    no_stroke()
+    ellipse(600, 150, size, size)
+    fill 0
+    umbra_x = 600 - (@moon_phase * size)
+
+    ellipse(umbra_x, 150-1, size+1, size+1)
   end
   
 end
